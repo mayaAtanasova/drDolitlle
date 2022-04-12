15c8b9bf63539286a958b22d46a00de7eb4ad5b0
Maya Atanasova <mayaatanasova@gmail.com>
2022-04-12 10:24:51 +0300

    created ad-form component

---
+++ src/app/feature/adlist/ad-form/ad-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-form',
+  templateUrl: './ad-form.component.html',
+  styleUrls: ['./ad-form.component.css']
+})
+export class AdFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/feature/adlist/adlist.module.ts
+++ src/app/feature/adlist/adlist.module.ts
@@ -5,13 +5,15 @@ import { AdlistRoutingModule } from './adlist-routing.module';
 import { AdNewComponent } from './ad-new/ad-new.component';
 import { AdDetailComponent } from './ad-detail/ad-detail.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AdFormComponent } from './ad-form/ad-form.component';
 
 
 
 @NgModule({
   declarations: [
     AdNewComponent,
-    AdDetailComponent
+    AdDetailComponent,
+    AdFormComponent
   ],
   imports: [
     CommonModule,
